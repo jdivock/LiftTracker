c293dfff856e3869af8488df4919da68bb0925b8
Jason Divock <jdivock@gmail.com>
2015-05-25 12:37:40 -0700

    Basic toolbar icons set up

--- index.ios.js
+++ index.ios.js
@@ -5,49 +5,80 @@
 'use strict';
 
 var React = require('react-native');
+
 var {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    TabBarIOS,
+    StatusBarIOS
 } = React;
 
-var LiftTracker = React.createClass({
-  render: function() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
+var styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF'
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5
+    }
 });
 
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+var LiftTracker = React.createClass({
+    getInitialState() {
+        return {
+            selectedTab: 'home'
+        };
+    },
+    changeTab(tabName){
+        StatusBarIOS.setStyle(tabName === 'home' ? 1 : 0);
+        this.setState({
+            selectedTab: tabName
+        });
+    },
+    render: function(){
+        return (
+            <TabBarIOS>
+                <TabBarIOS.Item title="LiftTracker"
+                                icon={ require('image!barbell') }
+                                onPress={ () =>
+                                         this.changeTab('home') }
+                                selected={this.state.selectedTab ===
+                                          'home'}>
+                    <Text>Home!</Text>
+                </TabBarIOS.Item>
+                <TabBarIOS.Item title="Profile"
+                                icon={ require('image!profile') }
+                                onPress={ () =>
+                                         this.changeTab('profile') }
+                                selected={this.state.selectedTab ===
+                                          'profile'}>
+                    <Text>Profile!</Text>
+                </TabBarIOS.Item>
+                <TabBarIOS.Item title="Settings"
+                                icon={require('image!settings')}
+                                onPress={ () =>
+                                         this.changeTab('settings') }
+                                selected={this.state.selectedTab ===
+                                          'settings'}>
+                    <View style={styles.pageView}>
+                        <Text>Settings</Text>
+                    </View>
+                </TabBarIOS.Item>
+            </TabBarIOS>
+        );
+    }
 });
+
 
 AppRegistry.registerComponent('LiftTracker', () => LiftTracker);
